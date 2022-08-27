b8f8708b37bc9a272682c43d3c294d9391a14e95
Daniel Yeh <dyeh1989@gmail.com>
2022-08-26 22:39:06 -0700

    recipe id prop

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -19,7 +19,7 @@ function Popular() {
     <div>
       {popular.map(recipe => {
       return (
-        <div>
+        <div key={recipe.id}>
           <p>{recipe.title}</p>
         </div>
       )
