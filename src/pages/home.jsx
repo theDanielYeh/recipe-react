2a2c47d4bc77d98e16b6761b286f1734ec0a0192
Daniel Yeh <dyeh1989@gmail.com>
2022-08-26 21:56:09 -0700

    npm install react scripts

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,2 +1,15 @@
 import Veggie from "../components/veggie";
 import Popular from "../components/popular";
+
+import React from 'react'
+
+function home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  )
+}
+
+export default home
