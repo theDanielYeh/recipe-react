645514e689fb46edf7950f5cf179abf903967935
Daniel Yeh <dyeh1989@gmail.com>
2022-08-29 17:42:40 -0700

    added styling for card

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -21,7 +21,13 @@ function Popular() {
   return (
     <Wrapper>
       <h3>Popular Picks</h3>
-        <Splide>
+        <Splide options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "5rem",
+        }}>
           {popular.map((recipe) => {
             return (
               <SplideSlide>
@@ -45,9 +51,32 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center:
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
