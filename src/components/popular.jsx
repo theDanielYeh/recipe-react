4e1cba7ec02ad3beaf0879092cd4599118169078
Daniel Yeh <dyeh1989@gmail.com>
2022-08-29 17:23:17 -0700

    import styled from styled components

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
 
@@ -19,13 +20,34 @@ function Popular() {
     <div>
       {popular.map(recipe => {
       return (
-        <div key={recipe.id}>
-          <p>{recipe.title}</p>
-        </div>
+        // <div key={recipe.id}>
+        //   <p>{recipe.title}</p>
+        // </div>
+        <Wrapper>
+          <h3>Popular Picks</h3>
+          {popular.map((recipe) => {
+            return (
+              <Card>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              </Card>
+            )
+          })}
+        </Wrapper>
       )
     })}
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+`;
 
 export default Popular
