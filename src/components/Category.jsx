fcb68b570f6d03f0adb2aae0aa5e1b7bc0ceea27
Daniel Yeh <dyeh1989@gmail.com>
2022-08-30 19:23:31 -0700

    added BrowserRouter

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,7 +7,25 @@ import React from 'react'
 
 function Category() {
   return (
-    <div>
+    // <List>
+    //   <NavLink to={'/cuisine/Italian'}>
+    //     <FaPizzaSlice />
+    //     <h4>Italian</h4>
+    //   </NavLink>
+    //   <NavLink to={'/cuisine/American'}>
+    //     <FaHamburger />
+    //     <h4>American</h4>
+    //   </NavLink>
+    //   <NavLink to={'/cuisine/Thai'}>
+    //     <GiNoodles />
+    //     <h4>Thai</h4>
+    //   </NavLink>
+    //   <NavLink to={'/cuisine/Japanese'}>
+    //     <GiChopsticks />
+    //     <h4>Japanese</h4>
+    //   </NavLink>
+    // </List>
+    <List>
       <div>
         <FaPizzaSlice />
         <h4>Italian</h4>
@@ -24,8 +42,14 @@ function Category() {
         <GiChopsticks />
         <h4>Japanese</h4>
       </div>
-    </div>
+    </List>
   )
 }
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`
 
 export default Category
