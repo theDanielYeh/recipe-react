e129606f2dab5033a690e1f256bf698ec37518da
Daniel Yeh <dyeh1989@gmail.com>
2022-08-30 19:34:47 -0700

    added routers, router, browserrouter

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,41 +7,23 @@ import React from 'react'
 
 function Category() {
   return (
-    // <List>
-    //   <NavLink to={'/cuisine/Italian'}>
-    //     <FaPizzaSlice />
-    //     <h4>Italian</h4>
-    //   </NavLink>
-    //   <NavLink to={'/cuisine/American'}>
-    //     <FaHamburger />
-    //     <h4>American</h4>
-    //   </NavLink>
-    //   <NavLink to={'/cuisine/Thai'}>
-    //     <GiNoodles />
-    //     <h4>Thai</h4>
-    //   </NavLink>
-    //   <NavLink to={'/cuisine/Japanese'}>
-    //     <GiChopsticks />
-    //     <h4>Japanese</h4>
-    //   </NavLink>
-    // </List>
     <List>
-      <div>
+      <NavLink to={'/cuisine/Italian'}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to={'/cuisine/American'}>
         <FaHamburger />
         <h4>American</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to={'/cuisine/Thai'}>
         <GiNoodles />
         <h4>Thai</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to={'/cuisine/Japanese'}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </div>
+      </NavLink>
     </List>
   )
 }
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import { Link, useParams } from 'react-router-dom'
 
 function Cuisine() {
+
+  const getCuisine = async (name) => {
+
+  }
+
   return (
     <div>Cuisine</div>
   )
