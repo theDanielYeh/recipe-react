eaa999d8ff9936b72b4035b6ad61da3150bcc7a3
Daniel Yeh <dyeh1989@gmail.com>
2022-08-31 21:07:00 -0700

    create fetch request for Cuisine and add useEffect

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 import { Link, useParams } from 'react-router-dom'
 
 function Cuisine() {
 
-  const getCuisine = async (name) => {
+  const [cuisine, setCuisine] = useState([]);
 
+  const getCuisine = async (name) => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=$(name)`)
+    const recipes = await data.json();
+    setCuisine(recipes.results);
   }
+
+  useEffect(() => {
+    getCuisine('italian')
+  }, [])
 
   return (
     <div>Cuisine</div>
